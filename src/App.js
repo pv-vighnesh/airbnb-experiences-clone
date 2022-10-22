fd043de72460cc7a654ee730637046aad6ecbe51
pv-vighnesh <pvighnesh.vishnu@gmail.com>
2022-10-22 18:32:36 +0530

    added experience section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Card from "./components/Card";
+import data from "./Data";
 import "./App.css";
 
 export default function App() {
+  console.log("data",data);
+  const cards = data.map((item) => {
+    return <Card 
+      img={item.coverImg}
+      title={item.title}
+      price={item.price}
+      location={item.location}
+      rating={item.stats.rating}
+      reviewCount={item.stats.reviewCount}
+    />
+  })
   return (
-
     <>
       <Navbar />
       <Hero />
+      <section className="cards-list">
+      {cards}
+      </section>
     </>
   )
 }
